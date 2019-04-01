bb00daea1cf7cf9f5627ade58ef4f0a9714bd08a
pyk <x24870@gmail.com>
2019-04-01 23:53:20 +0800

    Finish draft, waiting for test

--- BMC/spx_tool/release.py
+++ BMC/spx_tool/release.py
@@ -5,91 +5,134 @@ if 'linux' in subprocess.check_output(['uname']).lower():
 else:
     call_py = 'python2'
 
+class Releaser():
+    def __init__(self):
+        self.FW_ver = {'major': None, 'minor': None, 'aux': None}
+        self.SHA256 = ''
 
-def release(HPMfolder_path):
-    #make sure git is clean
-    en = raw_input('''
-    Please check following items:
-    1. Make sure your git repository is clean
-    2. Update FW version in PRJ file
-    3. Copy rom.ima to CreateHPM folder
-    
-    I finished all items above (y/n):
-    ''')
-    if en.upper() != 'Y':
-        print("Exit release process")
-        exit()
+    def release(self, HPMfolder_path):
+        #Check list
+        en = raw_input('''
+        Please check following items:
+        1. Make sure your git repository is clean
+        2. Update FW version in PRJ file
+        3. Copy rom.ima, private.pem to CreateHPM folder
+        
+        I finished all items above (y/n):
+        ''')
+        if en.upper() != 'Y':
+            print("Exit release process")
+            exit()
 
-    #get PRJ name
-    patch_PRJ, PRJ = get_PRJ_name()
+        #get PRJ name
+        patch_PRJ, PRJ = self.get_PRJ_name()
 
-    #clean PRJ patch
-    #clear_PRJ_patch(patch_PRJ, PRJ)
+        #clean PRJ patch
+        #self.clear_PRJ_patch(patch_PRJ, PRJ)
 
-    #update FW version
-    cur_FW_ver = get_cur_FW_ver(PRJ)
-    print(cur_FW_ver)
+        #Get FW version
+        self.FW_ver['major'], self.FW_ver['minor'], self.FW_ver['aux'] = self.get_cur_FW_ver(PRJ)
+        print("\nFW version: {}.{}.{}".format(self.FW_ver['major'], self.FW_ver['minor'], self.FW_ver['aux']))
 
-    #gen_SHA256('release.py')
+        #edit .conf for create HPM
+        #self.edit_HPM_conf(HPMfolder_path)
 
-    #edit_releaseNode()
+        #create HPM
+        #self.create_HPM()
 
+        #generate SHA256 code
+        self.SHA256 = self.gen_SHA256(os.path.join(HPMfolder_path, 'rom.ima'))#TODO: make sure returned string is specified
+        print('SHA256: {}'.format(self.SHA256))
 
-def gen_SHA256(filepath):
-    #generate SHA256
-    return subprocess.check_output(['sha256sum', filepath], shell=True)
-    
-def clear_PRJ_patch(patch_PRJ, PRJ):
-    #update PRJ
-    subprocess.call([call_py, 'patch_prj.py'])
+        #edit ReleasNode
+        #self.edit_releaseNode()
 
-    #clean config/patch
-    subprocess.call(['rm', os.path.join('..', 'configs', 'patch', '*')])
+        #git commit
+        #self.git_commit()
 
-    #replate origin .PRJ file to patched .PRJ file
-    subprocess.call(['mv', patch_PRJ, PRJ])
+        #git add tag
+        #self.git_add_tag()
 
-def get_PRJ_name():
-    PRJ = ''
-    patch_PRJ = ''
-    confing_folder = os.path.join('..', 'configs')
-    for filename in os.listdir(confing_folder):
-        if filename.endswith('_patch.PRJ'):
-            patch_PRJ = os.path.join(confing_folder, filename)
-            PRJ = os.path.join(confing_folder, filename[:-10] + '.PRJ') 
-    if not patch_PRJ:
-        print("Error: can't find patched .PRJ file")
-        exit()
-    return patch_PRJ, PRJ 
+        #finish
+        print('Finish!\nNow you can edit ReleaseNode and push to remote Git')
+        
+    def clear_PRJ_patch(self, patch_PRJ, PRJ):
+        #update PRJ
+        subprocess.call([call_py, 'patch_prj.py'])
 
-def get_cur_FW_ver(PRJ_path):
-    FW_ver = ''
-    with open(PRJ_path, 'r') as f:
-        for line in f.readlines():
-            if 'CONFIG_SPX_MAP_WOLFPASS_ATTR_Major' in line:
-                FW_ver += line.split('=')[1].replace('"', '').replace('\n', '') + '.'
-            elif 'CONFIG_SPX_MAP_WOLFPASS_ATTR_Minor' in line:
-                FW_ver += line.split('=')[1].replace('"', '').replace('\n', '') + '.'
-            elif 'CONFIG_SPX_MAP_WOLFPASS_ATTR_Aux' in line:
-                FW_ver += line.split('=')[1].replace('"', '').replace('\n', '')
-    return FW_ver
+        #clean config/patch
+        subprocess.call(['rm', os.path.join('..', 'configs', 'patch', '*')])
 
-def edit_releaseNode():
-    #edit release node
-    filename = os.path.join('..', 'ReleaseNode')
-    with open(filename, 'a') as f:
-        f.write('==========================================================================\n')
-        f.write('Version number: ')
-        f.write('Release Date: ' + datetime.datetime.now().strftime('%Y-%m-%d') + '\n')
-        f.write('SHA256 checksum:\n' + str(gen_SHA256('rom.ima')))
+        #replate origin .PRJ file to patched .PRJ file
+        subprocess.call(['mv', patch_PRJ, PRJ])
 
-#create HPM
+    def get_PRJ_name(self):
+        PRJ = ''
+        patch_PRJ = ''
+        confing_folder = os.path.join('..', 'configs')
+        for filename in os.listdir(confing_folder):
+            if filename.endswith('_patch.PRJ'):
+                patch_PRJ = os.path.join(confing_folder, filename)
+                PRJ = os.path.join(confing_folder, filename[:-10] + '.PRJ') 
+        if not patch_PRJ:
+            print("Error: can't find patched .PRJ file")
+            exit()
+        return patch_PRJ, PRJ 
 
-#git add
+    def get_cur_FW_ver(self, PRJ_path):
+        major, minor, aux = '', '', ''
+        with open(PRJ_path, 'r') as f:
+            for line in f.readlines():
+                if 'CONFIG_SPX_MAP_WOLFPASS_ATTR_Major' in line:
+                    major = line.split('=')[1].replace('"', '').replace('\n', '')
+                elif 'CONFIG_SPX_MAP_WOLFPASS_ATTR_Minor' in line:
+                    minor = line.split('=')[1].replace('"', '').replace('\n', '')
+                elif 'CONFIG_SPX_MAP_WOLFPASS_ATTR_Aux' in line:
+                    aux = line.split('=')[1].replace('"', '').replace('\n', '')
+        return major, minor, aux
 
-#git add tag
+    def edit_releaseNode(self):
+        #edit release node
+        filename = os.path.join('..', 'ReleaseNode')
+        with open(filename, 'a') as f:
+            f.write('==========================================================================\n')
+            f.write('Version number: ')
+            f.write('Release Date: ' + datetime.datetime.now().strftime('%Y-%m-%d') + '\n')
+            f.write('SHA256 checksum:\n' + str(gen_SHA256('rom.ima')))
 
-#push to git
+    def edit_HPM_conf(self, HPMfolder_path):
+        #TODO: use correct conf filename
+        HPMconf = 'signedImage.conf'
+        if HPMconf not in os.listdir(HPMfolder_path):
+            print("Error: Can't find '{}' in '{}'".format(HPMconf, HPMfolder_path))
+
+        with open(HPMconf, 'w') as f:
+            #TODO: edit HPMconf
+            pass
+                   
+    def create_HPM(self):
+        subprocess.call(['./CreateHPMImage', 'create', 'signedHPM.conf'])#TODO: use correct file name
+
+    def gen_SHA256(self, filepath):
+        #generate SHA256
+        return subprocess.check_output(['sha256sum', filepath], shell=True)
+
+    def git_commit(self):
+        #TODO: make sure git proccess is correct
+        subprocess.call(['git', 'add', os.path.join('..', 'configs')])
+        subprocess.call(['git', 'commit', '-m', 
+        '"Release Firmware version: {}.{}.{}"'.format(self.FW_ver['major'], self.FW_ver['minor'], self.FW_ver['aux'])])
+
+    def git_add_tag(self):
+        #TODO:verify if this command is correct
+        subprocess.call([
+            'git', 
+            'tag',
+            'Release ver:{}.{}.{}'.format(self.FW_ver['major'], self.FW_ver['minor'], self.FW_ver['aux']),
+            'HEAD'
+            ])
+
+    #push to git
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
@@ -102,4 +145,5 @@ if __name__ == '__main__':
     elif 'private.pem' not in os.listdir(sys.argv[1]) or 'rom.ima' not in os.listdir(sys.argv[1]):
         print("Error: Can't find 'private.pem' or 'rom.ima' in provided folder path")
     else:
-        release(sys.argv[1])
+        rlsr = Releaser()
+        rlsr.release(sys.argv[1])
